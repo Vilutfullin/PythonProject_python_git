d5844c4a38d694c929b06f5dfd39b8d5c0bacf1c
Vil Lutfullin <vil.lut@mail.ru>
2025-05-23 15:48:42 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from selene import browser, have
 def test_search():
     browser.open('https://ya.ru')
     browser.element('[name="text"]').type('qa.guru').press_enter()
-    browser.element('html').should(have.text('Курсы тестировщиков — обучение...'))
+    browser.element('html').should(have.text('Курсы тестировщиков'))
